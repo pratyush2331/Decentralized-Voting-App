54141eec32043c86174d788be6b1cc4be65c9eaa
Pratyush Raj <pratyush2331@gmail.com>
2024-02-12 23:28:49 +0530

    changed small alignment in smart contract

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -14,17 +14,17 @@ contract Voting {
     uint256 public votingStart;
     uint256 public votingEnd;
 
-constructor(string[] memory _candidateNames, uint256 _durationInMinutes) {
-    for (uint256 i = 0; i < _candidateNames.length; i++) {
-        candidates.push(Candidate({
-            name: _candidateNames[i],
-            voteCount: 0
-        }));
+    constructor(string[] memory _candidateNames, uint256 _durationInMinutes) {
+        for (uint256 i = 0; i < _candidateNames.length; i++) {
+            candidates.push(Candidate({
+                name: _candidateNames[i],
+                voteCount: 0
+            }));
+        }
+        owner = msg.sender;
+        votingStart = block.timestamp;
+        votingEnd = block.timestamp + (_durationInMinutes * 1 minutes);
     }
-    owner = msg.sender;
-    votingStart = block.timestamp;
-    votingEnd = block.timestamp + (_durationInMinutes * 1 minutes);
-}
 
     modifier onlyOwner {
         require(msg.sender == owner);
